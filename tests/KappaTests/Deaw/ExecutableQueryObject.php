6cb866858c07521ec308c43c1d68d693eadda404
Budry <zarubaondra@gmail.com>
2015-06-21 21:44:40 +0200

    Added example executable query for test

---
+++ tests/KappaTests/Deaw/ExecutableQueryObject.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Kappa\Deaw package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace KappaTests\Deaw;
+
+use Kappa\Deaw\Queries\Queryable;
+use Kappa\Deaw\Queries\QueryBuilder;
+
+/**
+ * Class SelectQueryObject
+ *
+ * @package KappaTests\Deaw
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class ExecutableQueryObject implements Queryable
+{
+	/**
+	 * @param QueryBuilder $builder
+	 * @return \DibiFluent
+	 */
+	public function getBuilder(QueryBuilder $builder)
+	{
+		$query = $builder->insert(['name' => 'bar']);
+
+		return $query;
+	}
+}
