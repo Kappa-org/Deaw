21659c20172042e4ee568d2e335b0f42202975f5
Budry <zarubaondra@gmail.com>
2015-06-21 22:46:32 +0200

    Added DI

---
+++ src/DI/DeawExtension.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of the Kappa\Deaw package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Deaw\DI;
+
+use Nette\DI\CompilerExtension;
+use Nette\DI\Statement;
+use Nette\Utils\Random;
+
+/**
+ * Class DeawExtension
+ *
+ * @package Kappa\Deaw\DI
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class DeawExtension extends CompilerExtension
+{
+	private $defaultConfig = [
+		'connection' => [],
+		'tables' => []
+	];
+
+	public function loadConfiguration()
+	{
+		$config = $this->getConfig($this->defaultConfig);
+		$builder = $this->getContainerBuilder();
+
+		$builder->addDefinition($this->prefix('dibiConnection'))
+			->setClass('\DibiConnection', [$config['connection']]);
+
+		$tableServices = [];
+		foreach ($config['tables'] as $tableName) {
+			$tableServices[] = new Statement('Kappa\Deaw\Table', [
+				$this->prefix("@dibiConnection"),
+				$tableName
+			]);
+		}
+
+		$builder->addDefinition($this->prefix('tableManager'))
+			->setClass('Kappa\Deaw\TableManager', [$tableServices]);
+	}
+}
---
+++ tests/KappaTests/Deaw/DI/DeawExtensionTest.phpt
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This file is part of the Kappa\Deaw package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ *
+ * @testCase
+ */
+
+namespace Kappa\Deaw\Tests;
+
+use Kappa\Deaw\TableManager;
+use Nette\DI\Container;
+use Tester\TestCase;
+use Tester\Assert;
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+/**
+ * Class DeawExtensionTest
+ *
+ * @package Kappa\Deaw\Tests
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class DeawExtensionTest extends TestCase
+{
+	/** @var Container */
+	private $container;
+
+	/**
+	 * @param Container $container
+	 */
+	public function __construct(Container $container)
+	{
+		$this->container = $container;
+	}
+
+	public function testTableManager()
+	{
+		$type = 'Kappa\Deaw\TableManager';
+		/** @var TableManager $service */
+		$service = $this->container->getByType($type);
+		Assert::type($type, $service);
+		Assert::type('Kappa\Deaw\Table', $service->getTable("user"));
+		Assert::type('Kappa\Deaw\Table', $service->create("user"));
+	}
+}
+
+\run(new DeawExtensionTest(getContainer()));
